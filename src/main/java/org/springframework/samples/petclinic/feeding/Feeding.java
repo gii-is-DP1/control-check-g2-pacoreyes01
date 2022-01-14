2e9d3295ef49eba61344bd8d7cb32b7f52776610
frareymad <frareymad@alum.us.es>
2022-01-14 12:25:06 +0100

    EXAMEN LAB DP1

--- src/main/java/org/springframework/samples/petclinic/feeding/Feeding.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/Feeding.java
@@ -2,6 +2,15 @@ package org.springframework.samples.petclinic.feeding;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.samples.petclinic.model.BaseEntity;
 import org.springframework.samples.petclinic.pet.Pet;
 
 import lombok.Getter;
@@ -9,9 +18,25 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class Feeding {
-    Integer id;
-    LocalDate startDate;
-    double weeksDuration;
-    Pet pet;   
+@Entity
+public class Feeding extends BaseEntity{
+    
+	@NotNull
+	@Column(name = "start_date")        
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
+	private LocalDate startDate;
+	
+	@NotNull
+	@Column(name = "weeks_duration")
+	@Min(1)
+	double weeksDuration;
+	
+	@ManyToOne
+	@NotNull
+	Pet pet; 
+	
+	@ManyToOne
+	@NotNull
+	@JoinColumn(name = "feeding_type_id")
+	FeedingType feedingType;
 }
